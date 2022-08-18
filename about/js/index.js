3af5ab30ef2011727341f2a67fdf442ffb5432e2
Luo Yihang <luo_yihang@outlook.com>
2022-08-18 11:58:18 +0800

    Site updated: 2022-08-18 11:58:18

---
+++ about/js/index.js
@@ -0,0 +1,381 @@
+"use strict";
+
+var configs = (function () {
+    var instance;
+    var Singleton = function (options) {
+        var options = options || Singleton.defaultOptions;
+        for (var key in Singleton.defaultOptions) {
+            this[key] = options[key] || Singleton.defaultOptions[key];
+        }
+    };
+    Singleton.defaultOptions = {
+        general_help: "Below there's a list of commands that you can use.\nYou can use autofill by pressing the TAB key, autocompleting if there's only 1 possibility, or showing you a list of possibilities.",
+        ls_help: "List information about the files and folders (the current directory by default).",
+        cat_help: "Read FILE(s) content and print it to the standard output (screen).",
+        whoami_help: "Print the user name associated with the current effective user ID",
+        date_help: "Print the system date and time.",
+        help_help: "Print this menu.",
+        clear_help: "Clear the terminal screen.",
+        reboot_help: "Reboot the system.",
+        exit_help: "Exit the system",
+        cd_help: "Change the current working directory.",
+        mv_help: "Move (rename) files.",
+        rm_help: "Remove files or directories.",
+        rmdir_help: "Remove directory, this command will only work if the folders are empty.",
+        touch_help: "Change file timestamps. If the file doesn't exist, it's created an empty one.",
+        sudo_help: "Execute a command as the superuser.",
+        welcome: "Congratulations!\n You have hacked the the terminal of Dark Hole. You can get more information about Luo Yihang. Try to type 'help' for the insturctions.",
+        invalid_command_message: "<value>: command not found.",
+        permission_denied_message: "Unable to '<value>', permission denied.",
+        sudo_message: "Permission denied.",
+        usage: "Usage",
+        file: "file",
+        file_not_found: "File '<value>' not found.",
+        value_token: "<value>",
+        host: "darklyh.com",
+        user: "guest",
+        is_root: false,
+        type_delay: 20
+    };
+    return {
+        getInstance: function (options) {
+            instance === void 0 && (instance = new Singleton(options));
+            return instance;
+        }
+    };
+})();
+
+var files = (function () {
+    var instance;
+    var Singleton = function (options) {
+        var options = options || Singleton.defaultOptions;
+        for (var key in Singleton.defaultOptions) {
+            this[key] = options[key] || Singleton.defaultOptions[key];
+        }
+    };
+    Singleton.defaultOptions = {
+        "about.txt": "罗奕航-新加坡南洋理工大学-计算机科学",
+        "why_this_blog_exists.txt": "https://darklyh.com/%E9%9A%8F%E7%AC%94/%E4%BD%A0%E5%A5%BD%EF%BC%8C%E4%B8%96%E7%95%8C",
+        "contact.txt": "luo_yihang@example.com",
+        "github.txt": "https://github.com/Luo-Yihang",
+    };
+    return {
+        getInstance: function (options) {
+            instance === void 0 && (instance = new Singleton(options));
+            return instance;
+        }
+    };
+})();
+
+var main = (function () {
+
+    var ignoreEvent = function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+    };
+    
+    var scrollToBottom = function () {
+        document.getElementById('input-line').scrollIntoView({block: 'end' });
+    };
+    
+    var isURL = function (str) {
+        return (str.startsWith("http") || str.startsWith("www")) && str.indexOf(" ") === -1 && str.indexOf("\n") === -1;
+    };
+    
+    /**
+     * Model
+     */
+    var InvalidArgumentException = function (message) {
+        this.message = message;
+        // Use V8's native method if available, otherwise fallback
+        if ("captureStackTrace" in Error) {
+            Error.captureStackTrace(this, InvalidArgumentException);
+        } else {
+            this.stack = (new Error()).stack;
+        }
+    };
+    // Extends Error
+    InvalidArgumentException.prototype = Object.create(Error.prototype);
+    InvalidArgumentException.prototype.name = "InvalidArgumentException";
+    InvalidArgumentException.prototype.constructor = InvalidArgumentException;
+
+    var cmds = {
+        LS: { value: "ls", help: configs.getInstance().ls_help },
+        CAT: { value: "cat", help: configs.getInstance().cat_help },
+        WHOAMI: { value: "whoami", help: configs.getInstance().whoami_help },
+        DATE: { value: "date", help: configs.getInstance().date_help },
+        HELP: { value: "help", help: configs.getInstance().help_help },
+        CLEAR: { value: "clear", help: configs.getInstance().clear_help },
+        REBOOT: { value: "reboot", help: configs.getInstance().reboot_help },
+        EXIT: {value: "exit", help: configs.getInstance.exit_help },
+        CD: { value: "cd", help: configs.getInstance().cd_help },
+        MV: { value: "mv", help: configs.getInstance().mv_help },
+        RM: { value: "rm", help: configs.getInstance().rm_help },
+        RMDIR: { value: "rmdir", help: configs.getInstance().rmdir_help },
+        TOUCH: { value: "touch", help: configs.getInstance().touch_help },
+        SUDO: { value: "sudo", help: configs.getInstance().sudo_help }
+    };
+
+    var Terminal = function (prompt, cmdLine, output, user, host, root, outputTimer) {
+        if (!(prompt instanceof Node) || prompt.nodeName.toUpperCase() !== "DIV") {
+            throw new InvalidArgumentException("Invalid value " + prompt + " for argument 'prompt'.");
+        }
+        if (!(cmdLine instanceof Node) || cmdLine.nodeName.toUpperCase() !== "INPUT") {
+            throw new InvalidArgumentException("Invalid value " + cmdLine + " for argument 'cmdLine'.");
+        }
+        if (!(output instanceof Node) || output.nodeName.toUpperCase() !== "DIV") {
+            throw new InvalidArgumentException("Invalid value " + output + " for argument 'output'.");
+        }
+        (typeof user === "string" && typeof host === "string") && (this.completePrompt = user + "@" + host + ":~" + (root ? "#" : "$"));
+        this.prompt = prompt;
+        this.cmdLine = cmdLine;
+        this.output = output;
+        this.typeSimulator = new TypeSimulator(outputTimer, output);
+    };
+
+    Terminal.prototype.type = function (text, callback) {
+        this.typeSimulator.type(text, callback);
+    };
+
+    Terminal.prototype.exec = function () {
+        var command = this.cmdLine.value;
+        this.cmdLine.value = "";
+        this.prompt.textContent = "";
+        this.output.innerHTML += "<span class=\"prompt-color\">" + this.completePrompt + "</span> " + command + "<br/>";
+    };
+
+    Terminal.prototype.init = function () {
+        this.cmdLine.disabled = true;
+        this.cmdLine.addEventListener("keydown", function (event) {
+            if (event.which === 13 || event.keyCode === 13) {
+                this.handleCmd();
+                ignoreEvent(event);
+            } else if (event.which === 9 || event.keyCode === 9) {
+                this.handleFill();
+                ignoreEvent(event);
+            }
+        }.bind(this));
+        this.reset();
+    };
+
+    Terminal.makeElementDisappear = function (element) {
+        element.style.opacity = 0;
+        element.style.transform = "translateX(-300px)";
+    };
+
+    Terminal.makeElementAppear = function (element) {
+        element.style.opacity = 1;
+        element.style.transform = "translateX(0)";
+    };
+
+    Terminal.prototype.lock = function () {
+        this.exec();
+        this.cmdLine.blur();
+        this.cmdLine.disabled = true;
+    };
+
+    Terminal.prototype.unlock = function () {
+        this.cmdLine.disabled = false;
+        this.prompt.textContent = this.completePrompt;
+        scrollToBottom();
+        this.focus();
+    };
+
+    Terminal.prototype.handleFill = function () {
+        var cmdComponents = this.cmdLine.value.trim().split(" ");
+        if ((cmdComponents.length <= 1) || (cmdComponents.length === 2 && cmdComponents[0] === cmds.CAT.value)) {
+            var possibilities = [];
+            if (cmdComponents[0].toLowerCase() === cmds.CAT.value) {
+                if (cmdComponents.length === 1) {
+                    cmdComponents[1] = "";
+                }
+                for (var file in files.getInstance()) {
+                    if (file.startsWith(cmdComponents[1].toLowerCase())) {
+                        possibilities.push(cmds.CAT.value + " " + file);
+                    }
+                }
+            } else {
+                for (var command in cmds) {
+                    if (cmds[command].value.startsWith(cmdComponents[0].toLowerCase())) {
+                        possibilities.push(cmds[command].value);
+                    }
+                }
+            }
+
+            if (possibilities.length === 1) {
+                console.log(this.cmdLine.value)
+                console.log(possibilities[0] + " ")
+                this.cmdLine.value = possibilities[0] + " ";
+            } else if (possibilities.length > 1) {
+                this.lock();
+                this.type(possibilities.join("\n"), function () {
+                    this.cmdLine.value = cmdComponents.join(" ");
+                    this.unlock();
+                }.bind(this));
+            }
+        }
+    };
+
+    Terminal.prototype.handleCmd = function () {
+        var cmdComponents = this.cmdLine.value.trim().split(" ");
+        this.lock();
+        switch (cmdComponents[0]) {
+            case cmds.CAT.value:
+                this.cat(cmdComponents);
+                break;
+            case cmds.LS.value:
+                this.ls();
+                break;
+            case cmds.WHOAMI.value:
+                this.whoami();
+                break;
+            case cmds.DATE.value:
+                this.date();
+                break;
+            case cmds.HELP.value:
+                this.help();
+                break;
+            case cmds.CLEAR.value:
+                this.clear();
+                break;
+            case cmds.REBOOT.value:
+                this.reboot();
+                break;
+            case cmds.EXIT.value:
+                this.exit();
+                break;
+            case cmds.CD.value:
+            case cmds.MV.value:
+            case cmds.RMDIR.value:
+            case cmds.RM.value:
+            case cmds.TOUCH.value:
+                this.permissionDenied(cmdComponents);
+                break;
+            case cmds.SUDO.value:
+                this.sudo();
+                break;
+            default:
+                this.invalidCommand(cmdComponents);
+                break;
+        };
+    };
+
+    Terminal.prototype.cat = function (cmdComponents) {
+        var result;
+        if (cmdComponents.length <= 1) {
+            result = configs.getInstance().usage + ": " + cmds.CAT.value + " <" + configs.getInstance().file + ">";
+        } else if (!cmdComponents[1] || (!files.getInstance().hasOwnProperty(cmdComponents[1]))) {
+            result = configs.getInstance().file_not_found.replace(configs.getInstance().value_token, cmdComponents[1]);
+        } else {
+            result = files.getInstance()[cmdComponents[1]];
+        }
+        this.type(result, this.unlock.bind(this));
+    };
+
+    Terminal.prototype.ls = function () {
+        var result = ""
+        for (var file in files.getInstance()) {
+            result += file + "\n";
+        }
+        this.type(result.trim(), this.unlock.bind(this));
+    };
+
+    Terminal.prototype.sudo = function () {
+        this.type(configs.getInstance().sudo_message, this.unlock.bind(this));
+    }
+
+    Terminal.prototype.whoami = function (cmdComponents) {
+        var result = configs.getInstance().user + "\n";
+        this.type(result, this.unlock.bind(this));
+    };
+
+    Terminal.prototype.date = function (cmdComponents) {
+        this.type(new Date().toString(), this.unlock.bind(this));
+    };
+
+    Terminal.prototype.help = function () {
+        var result = configs.getInstance().general_help + "\n\n";
+        for (var cmd in cmds) {
+            result += cmds[cmd].value + " - " + cmds[cmd].help + "\n";
+        }
+        this.type(result.trim(), this.unlock.bind(this));
+    };
+
+    Terminal.prototype.clear = function () {
+        this.output.textContent = "";
+        this.prompt.textContent = "";
+        this.prompt.textContent = this.completePrompt;
+        this.unlock();
+    };
+
+    Terminal.prototype.reboot = function () {
+        location.reload();
+    };
+
+    Terminal.prototype.exit = function () {
+        history.back()
+    }
+
+    Terminal.prototype.reset = function () {
+        this.output.textContent = "";
+        this.prompt.textContent = "";
+        if (this.typeSimulator) {
+            this.type(configs.getInstance().welcome + "", function () {
+                this.unlock();
+            }.bind(this));
+        }
+    };
+
+    Terminal.prototype.permissionDenied = function (cmdComponents) {
+        this.type(configs.getInstance().permission_denied_message.replace(configs.getInstance().value_token, cmdComponents[0]), this.unlock.bind(this));
+    };
+
+    Terminal.prototype.invalidCommand = function (cmdComponents) {
+        this.type(configs.getInstance().invalid_command_message.replace(configs.getInstance().value_token, cmdComponents[0]), this.unlock.bind(this));
+    };
+
+    Terminal.prototype.focus = function () {
+        this.cmdLine.focus();
+    };
+
+    var TypeSimulator = function (timer, output) {
+        var timer = parseInt(timer);
+        if (timer === Number.NaN || timer < 0) {
+            throw new InvalidArgumentException("Invalid value " + timer + " for argument 'timer'.");
+        }
+        if (!(output instanceof Node)) {
+            throw new InvalidArgumentException("Invalid value " + output + " for argument 'output'.");
+        }
+        this.timer = timer;
+        this.output = output;
+    };
+
+    TypeSimulator.prototype.type = function (text, callback) {
+        if (isURL(text)) {
+            window.open(text);
+        }
+
+        var output = this.output;
+
+        output.innerHTML += (text.substring(0).replace(new RegExp("\n", 'g'), "<br/>")) + "<br/>";
+        callback();
+
+        scrollToBottom();
+    };
+
+    return {
+        listener: function () {
+            new Terminal(
+                document.getElementById("prompt"),
+                document.getElementById("cmdline"),
+                document.getElementById("output"),
+                configs.getInstance().user,
+                configs.getInstance().host,
+                configs.getInstance().is_root,
+                configs.getInstance().type_delay
+            ).init();
+        }
+    };
+})();
+
+window.onload = main.listener;
